ad744aa07a408e950f412b56cba52471bba81f6a
21092004Goda <samb21092004@gmail.com>
2025-12-12 21:53:07 +0300

    По приколу
    # Conflicts:
    #	src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt

--- src/main/kotlin/ru/quipy/apigateway/APIController.kt
+++ src/main/kotlin/ru/quipy/apigateway/APIController.kt
@@ -60,10 +60,10 @@ class APIController(
 
     @PostMapping("/orders/{orderId}/payment")
     fun payOrder(@PathVariable orderId: UUID, @RequestParam deadline: Long): PaymentSubmissionDto {
-        if (!rateLimiter.tick()) {
-            val retryAfterMs = 10L + Random.nextLong(10)
-            throw TooManyRequestsException(retryAfterMs)
-        }
+//        if (!rateLimiter.tick()) {
+//            val retryAfterMs = 10L + Random.nextLong(10)
+//            throw TooManyRequestsException(retryAfterMs)
+//        }
         val paymentId = UUID.randomUUID()
         val order = orderRepository.findById(orderId)?.let {
             orderRepository.save(it.copy(status = OrderStatus.PAYMENT_IN_PROGRESS))
