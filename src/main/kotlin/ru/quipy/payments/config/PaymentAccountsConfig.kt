8cfe048280da5450c00f4a703376687748540287
21092004Goda <samb21092004@gmail.com>
2025-12-12 21:12:00 +0300

    По приколу
    # Conflicts:
    #	src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt

--- src/main/kotlin/ru/quipy/payments/config/PaymentAccountsConfig.kt
+++ src/main/kotlin/ru/quipy/payments/config/PaymentAccountsConfig.kt
@@ -45,7 +45,7 @@ class PaymentAccountsConfig {
     @Bean
     fun rateLimit(): SlidingWindowRateLimiter {
         return SlidingWindowRateLimiter(
-            rate = 1100L,
+            rate = 1000L,
             window = Duration.ofMillis(1000),
         )
     }
